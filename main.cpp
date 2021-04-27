cf859a0c8a5f8cfe8bc163676eb862367a2b81ed
regelo <regelo@regelo.org>
2021-04-27 13:24:57 -0400

    Retour au calcul du carré, mais on va maintenant jusqu'à 100 itérations

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 int main() {
     std::cout << "Salut le monde !" << std::endl;
-    int variable = 50;
+    int variable = 100;
     for(int i=0 ; i<variable ; i++) {
-        std::cout << i*i*i << std::endl;
+        std::cout << i*i << std::endl;
     }
     double autre_variable = 5.4;
     std::cout << autre_variable << std::endl;
